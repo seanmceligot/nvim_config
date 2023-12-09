2686a2844adc00a40d49b92a627ba112b278bae5
Sean McEligot <sean.mceligot@gmail.com>
2023-12-09 15:17:12 -0500

    lsp keybindings

--- init.lua
+++ init.lua
@@ -14,6 +14,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- mason :Mason
+local mason_plugin = {
+    "williamboman/mason.nvim",
+    'williamboman/mason-lspconfig.nvim'
+}
+
+-- lsp  :LspInfo
+-- neodev for editing .config/nvim/init.lua
+local lspconfig_plugin  = {
+	"neovim/nvim-lspconfig",
+	"folke/neodev.nvim",
+}
+
 -- which key :WhichKey
 local which_key_plugin = {
     "folke/which-key.nvim",
@@ -25,10 +38,6 @@ local which_key_plugin = {
     opts = { }
 }
 
--- mason :Mason
-local mason_plugin = {
-    "williamboman/mason.nvim"
-}
 -- diffview :DiffViewOpen
 local diffview_plugin = {
     'sindrets/diffview.nvim'
@@ -39,17 +48,65 @@ local treesitter_plugin = {
 'nvim-treesitter/nvim-treesitter'
 , 'nvim-treesitter/nvim-treesitter-textobjects'
 }
-local plugins = { mason_plugin, which_key_plugin, diffview_plugin, treesitter_plugin }
 
 -- lazy
+local plugins = { mason_plugin, which_key_plugin, diffview_plugin, treesitter_plugin, lspconfig_plugin }
 require("lazy").setup(plugins)
--- mason
+
+-- mason and lsp
+require("neodev").setup()
 require("mason").setup()
+require("mason-lspconfig").setup()
+local lspconfig = require('lspconfig')
+
+-- :h mason-lspconfig-automatic-server-setup
+require("mason-lspconfig").setup_handlers {
+function (server_name) -- default handler (optional)
+    lspconfig[server_name].setup {}
+end,
+}
+
 -- which key
 local wk = require("which-key")
 local wk_mappings = {
-   d = { "<cmd>DiffviewOpen<cr>", "DiffView" }, 
-   w = { "<cmd>WhichKey<cr>", "WhichKey" }, 
-   m = { "<cmd>Mason<cr>", "Mason" }, 
+   d = { "<cmd>DiffviewOpen<cr>", "DiffView" },
+   w = { "<cmd>WhichKey<cr>", "WhichKey" },
+   m = { "<cmd>Mason<cr>", "Mason" },
+   l = { "<cmd>LspInfo<cr>", "LspInfo" },
 }
 wk.register(wk_mappings, { prefix = "<leader>" })
+
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+callback = function(ev)
+-- Enable completion triggered by <c-x><c-o>
+vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+-- Buffer local mappings.
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+local opts = { buffer = ev.buf }
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+vim.keymap.set('n', '<space>wl', function()
+print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, opts)
+vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+vim.keymap.set('n', '<space>f', function()
+vim.lsp.buf.format { async = true }
+end, opts)
+end,
+})
+
