1e9ff16ade4107c1344ee9fca2747b2c7bbadefe
Sean McEligot <sean.mceligot@gmail.com>
2023-12-09 13:08:35 -0500

    treesitter  and whichkey menu

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
+-- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -13,7 +14,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
+-- which key :WhichKey
 local which_key_plugin = {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -21,28 +22,34 @@ local which_key_plugin = {
         vim.o.timeout = true
         vim.o.timeoutlen = 100
     end,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    }
+    opts = { }
 }
 
+-- mason :Mason
 local mason_plugin = {
     "williamboman/mason.nvim"
 }
+-- diffview :DiffViewOpen
+local diffview_plugin = {
+    'sindrets/diffview.nvim'
+}
 
-local plugins = { mason_plugin, which_key_plugin }
+-- TreeSitter :TSInstall
+local treesitter_plugin = {
+'nvim-treesitter/nvim-treesitter'
+, 'nvim-treesitter/nvim-treesitter-textobjects'
+}
+local plugins = { mason_plugin, which_key_plugin, diffview_plugin, treesitter_plugin }
 
+-- lazy
 require("lazy").setup(plugins)
-
+-- mason
 require("mason").setup()
-
+-- which key
 local wk = require("which-key")
 local wk_mappings = {
-  v = { ":visual<CR>", "Enter Visual Mode" },
-  d = { ":delete<CR>", "Delete" },
-  c = { ":change<CR>", "Change" },
-  -- Add more mappings as needed
+   d = { "<cmd>DiffviewOpen<cr>", "DiffView" }, 
+   w = { "<cmd>WhichKey<cr>", "WhichKey" }, 
+   m = { "<cmd>Mason<cr>", "Mason" }, 
 }
 wk.register(wk_mappings, { prefix = "<leader>" })
